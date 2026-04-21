242845e22da8c883c113c06198c97d07d252ceaa
Yoyo434 <yehyahorbali853@gmail.com>
2026-04-21 18:15:18 +0300

    v8

---
+++ SQL/main.sql
@@ -0,0 +1,20 @@
+create table if not exists Salesman(
+salesman_id int primary key,
+name varchar(50),
+city varchar(50),
+commission decimal(4,2)
+);
+
+INSERT INTO Salesman (salesman_id, name, city, commission) VALUES
+(5001, 'James Hoog', 'New York', 0.15),
+(5002, 'Nail Knite', 'Paris', 0.13),
+(5005, 'Pit Alex', 'London', 0.11),
+(5006, 'Mc Lyon', 'Paris', 0.14),
+(5007, 'Paul Adam', 'Rome', 0.13),
+(5003, 'Lauson Hen', 'San Jose', 0.12);
+
+select * from Salesman;
+
+select * from Salesman where city='Paris';
+
+
